828028cfedac30df861c0049699bbdc07189b91c
Marco Segura <marco@leadid.net>
2020-01-17 00:45:33 -0800

    Finished port display, might speef up tomorrow night

--- portfolio.php
+++ portfolio.php
@@ -18,6 +18,7 @@ include('includes/functions.php');
 <link href="css/print.css" rel="stylesheet" type="text/css" media="print" />
 <link href='https://fonts.googleapis.com/css?family=Lato:400,700,300' rel='stylesheet' type='text/css'>
 <link href="css/style.css" rel="stylesheet" type="text/css"  media="screen" />
+
 </head>
 
 
@@ -32,12 +33,8 @@ include('includes/functions.php');
   <section id="portfolio" class="sec-parallax">
               <h1 class="title">A few things I've worked on</h1>
   </section>
-  <section class="">
-      <h2>Galleries coming soon</h2>
-    </section>
   <section class="portfolio-wrapper container loading d-flex flex-column">
-
-    </section>
+  </section>
 
   </main>
   <?php
@@ -56,7 +53,7 @@ include('includes/functions.php');
           $.each(projectsList, function(index){
             //console.log(projectsList[index].id);
             project =  '<div class="project row d-flex flex-column justify-content-center">';
-            project += '<div class="thumb align-self-center"><img class="w-100 circle" src="images/portfolio/thumbs/'+projectsList[index].images[0].toLowerCase()+'" alt="'+projectsList[index].name+'"/></div>';
+            project += '<div class="thumb align-self-center"><img class="w-100 circle" src="images/portfolio/thumbs/'+projectsList[index].thumb.toLowerCase()+'" alt="'+projectsList[index].name+'"/></div>';
             project += '<h2 class="secondary-title align-self-center">'+projectsList[index].name+'</h3>';
             project += '<div class="p-body lead"></div>';
             project += '<button class="toggle fas fa-expand-alt" value="expand" data-index="'+projectsList[index].id+'"></button>';
@@ -71,23 +68,52 @@ include('includes/functions.php');
         })
         .done(function() {
             $('.toggle').click(function(){
-            let dataHolder = [];
+
+              let dataHolder = [];
               var index = this.dataset.index;
+              let dad = $(this).parent();
+
               if(this.value == 'expand'){
-                $(this).parent().addClass('expanded');
+                dad.addClass('expanded');
                 $(this).removeClass('fa-expand-alt').addClass('fa-compress-alt');
                 $.getJSON("projects/projects.json", function (data) {
-                    console.log(data.projects[index]);
-                    dataHolder = data.projects[index].description;
-                    $('.expanded .p-body').append(dataHolder).hide().fadeIn(1000);
-                }).done(function(){
-                  //$(this).parent().children('.p-body')
+                    dataHolder = '<div class="gallery d-flex flex-row justify-content-center">';
 
+                    if(data.projects[index].images !== '' || typeof data.projects[index].images !== 'undefined'){
+                      $.each(data.projects[index].images, function(i){
+                          console.log(data.projects[index].images[i]);
+                          dataHolder += '<img src="images/portfolio/'+data.projects[index].path+'/'+data.projects[index].images[i]+'"/>';
+                      });
+                    };
+                    dataHolder += '</div>';
+                    dataHolder += data.projects[index].description;
+                    dataHolder += '<div class="row">';
+                    dataHolder += '<div class="col-4">';
+                    dataHolder += '<h3>languages</h3>';
+                    $.each(data.projects[index].highlights[0].languages, function(i){
+                        dataHolder += '<span class="badge badge-pill badge-primary p-2 mr-2">'+data.projects[index].highlights[0].languages[i]+'</span>';
+                    })
+                    dataHolder += '</div>';
+                    dataHolder += '<div class="col-4">';
+                    dataHolder += '<h3>frameworks</h3>';
+                    $.each(data.projects[index].highlights[0].frameworks, function(i){
+                        dataHolder += '<span class="badge badge-pill badge-primary p-2 mr-2">'+data.projects[index].highlights[0].frameworks[i]+'</span>';
+                    })
+                    dataHolder += '</div>';
+                    dataHolder += '<div class="col-4">';
+                    dataHolder += '<h3>skills</h3>';
+                    $.each(data.projects[index].highlights[0].skills, function(i){
+                        dataHolder += '<span class="badge badge-pill badge-primary p-2 mr-2">'+data.projects[index].highlights[0].skills[i]+'</span>';
+                    })
+                    dataHolder += '</div>';
+
+                }).done(function(){
+                    dad.children('.expanded .p-body').append(dataHolder).hide().fadeIn(1000);
                 })
                 this.value = 'collapse';
               }else{
-                $('.expanded .p-body').html('');
-                $(this).parent().removeClass('expanded');
+                dad.children('.expanded .p-body').fadeOut(500, function(){$(this).html('')});
+                dad.removeClass('expanded');
                 $(this).removeClass('fa-compress-alt').addClass('fa-expand-alt');
                 this.value = 'expand';
               }
